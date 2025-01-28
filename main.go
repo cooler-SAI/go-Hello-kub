3854387f8c878e603d6b10887dbdc8614ffd6f97
cooler-SAI <cooler19860212@yahoo.com>
2025-01-28 22:21:10 +0300

    update goroutine, set up files

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"go-Hello-kub/handlers"
+	"go-Hello-kub/tools"
 	"net/http"
 	"os"
 )
@@ -14,9 +16,17 @@ func main() {
 
 	log.Info().Msg("Loading Simple Server...")
 	http.HandleFunc("/", handlers.HelloHandler)
-	log.Info().Msg("Server is running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return
+
+	server := &http.Server{
+		Addr: ":8080",
 	}
+
+	go func() {
+		log.Info().Msg("Server is running on http://localhost:8080")
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("Server error")
+		}
+	}()
+
+	tools.StopApp(server)
 }
--- tools/stop.go
+++ tools/stop.go
@@ -1,16 +1,28 @@
 package tools
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
+	"time"
 )
 
-func StopApp() {
-
+func StopApp(server *http.Server) {
 	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	<-stop
 
-	log.Println("Shutting down...")
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown error: %v", err)
+	}
+
+	log.Println("Server stopped")
 }
